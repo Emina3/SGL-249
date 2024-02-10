94186277fd2e1068e280ab145206a803fa0297ba
Emina Bayazitli <eminabayazitli34@gmail.com>
2024-02-10 19:13:25 +0100

    v1

---
+++ day-01.sql
@@ -0,0 +1,36 @@
+--HOMEWORK 1--
+/*
+1) Tablo Oluşturma
+
+filmler isminde bir tablo oluşturunuz.
+film_isim(varchar(50)), tür(varchar(20)), 
+süre(real), imdb(numeric(2,1)) 
+sütunları olsun. 
+
+
+2)  Data ekleme
+a-filmler tablosuna tüm fieldlarıyla 3 tane kayıt ekleyiniz.
+b-“ ogretmenler” isminde tablo olusturun.
+Icinde “kimlik_no”, “isim”, “brans” ve “cinsiyet” field’lari olsun
+(uygun data tiplerini kullanınız.).
+“ogretmenler” tablosuna bilgileri asagidaki gibi olan kisileri ekleyin.
+kimlik_no: 234431223, isim: Ayse Guler, brans : Matematik, cinsiyet: kadin.
+kimlik_no: 234431224, isim: Ali Guler, brans : Fizik, cinsiyet: erkek.
+
+
+3)Var olan tablodan yeni tablo oluşturmak 
+ “film_imdb” isminde bir tabloyu  “filmler” tablosundan oluşturunuz.
+İçinde “film_isim” ve “imdb” field’lari olsun
+
+
+
+4) bazı fieldları olan kayıt ekleme
+a-"filmler" tablosuna 
+film_isim:"Ayla", “film_imdb”:9.87,
+film_isim:"Shrek", “film_imdb”:9.83
+olan kayıtları ekleyiniz.
+
+b-“ogretmenler” tablosuna bilgileri 
+kimlik_no: 567597624, isim: Veli Guler
+olan kişiyi ekleyiniz.
+*/
